d6fe15bd72d856fd304a511937bde95970b7ee54
luisabwk <luisa.barwinski@gmail.com>
2024-11-19 12:04:29 -0300

    Update app.js
    Atualizei o código para utilizar as novas classes fornecidas:
    
    Local: .tvm__text.tvm__text--low-emphasis
    Tipo de Trabalho: .job-details-jobs-unified-top-card__job-insight.job-details-jobs-unified-top-card__job-insight--highlight
    Descrição: opção 1 usando CSS (.jobs-box__html-content.jobs-description-content__text.t-14.t-normal.jobs-description-content__text--stretch), ou a opção 2 usando o ID (#job-details)
    URL de Candidatura: .jobs-apply-button.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view

--- app.js
+++ app.js
@@ -243,10 +243,11 @@ async function getJobDetails(li_at, jobLink) {
     jobDetails = await page.evaluate(() => {
       const title = document.querySelector(".t-24")?.innerText.trim() || "";
       const company = document.querySelector(".job-card-container__company-name")?.innerText.trim() || "";
-      const location = document.querySelector("[class*='tvm__text--low-emphasis']")?.innerText.trim() || "";
-      const jobType = document.querySelector(".ivm-view-attr__img-wrapper")?.innerText.trim() || "";
-      const jobDescription = document.querySelector(".jobs-description-content__text")?.innerText.trim() || "";
-      const applyUrl = document.querySelector("a[data-control-name='jobdetails_topcard_inapply']")?.href || "";
+      const location = document.querySelector(".tvm__text.tvm__text--low-emphasis")?.innerText.trim() || "";
+      const jobType = document.querySelector(".job-details-jobs-unified-top-card__job-insight.job-details-jobs-unified-top-card__job-insight--highlight")?.innerText.trim() || "";
+      const jobDescription = document.querySelector(".jobs-box__html-content.jobs-description-content__text.t-14.t-normal.jobs-description-content__text--stretch")?.innerText.trim() ||
+                            document.querySelector("#job-details")?.innerText.trim() || "";
+      const applyUrl = document.querySelector(".jobs-apply-button.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view")?.href || "";
 
       return {
         vaga: title,
