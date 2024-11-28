0372270506127cf3fd2823cbe00e6408bdd20a15
luisabwk <luisa.barwinski@gmail.com>
2024-11-28 12:15:45 -0300

    Update scrape-jobs.js

--- scrape-jobs.js
+++ scrape-jobs.js
@@ -5,13 +5,15 @@ async function getJobListings(browser, searchTerm, location, li_at) {
   let allJobs = [];
   const baseUrl = `https://www.linkedin.com/jobs/search?keywords=${encodeURIComponent(
     searchTerm
-  )}&location=${encodeURIComponent(location)}&geoId=106057199&f_TPR=r86400`;
+  )}&location=${encodeURIComponent(
+    location
+  )}&geoId=106057199&f_TPR=r86400`;
 
   console.log(`[INFO] Acessando a URL inicial: ${baseUrl}`);
 
   const page = await browser.newPage();
 
-  // Definir o cookie `li_at` com o valor fornecido
+  // Define o cookie `li_at` com o valor fornecido
   const cookies = [
     {
       name: "li_at",
@@ -45,7 +47,9 @@ async function getJobListings(browser, searchTerm, location, li_at) {
 
     // Iterar sobre cada página de 1 até o total de páginas
     for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-      console.info(`[INFO] Scraping página ${currentPage} de ${totalPages}...`);
+      console.info(
+        `[INFO] Scraping página ${currentPage} de ${totalPages}...`
+      );
 
       // Navegar para a página específica
       const pageURL = `${baseUrl}&start=${(currentPage - 1) * 25}`;
@@ -76,10 +80,20 @@ async function getJobListings(browser, searchTerm, location, li_at) {
 
           const link = job.querySelector("a")?.href;
 
+          const format = job
+            .querySelector(".job-card-container__workplace-type")
+            ?.innerText.trim();
+
+          const cargahoraria = job
+            .querySelector(".job-card-container__employment-status")
+            ?.innerText.trim();
+
           return {
             vaga: title || "",
             empresa: company || "",
             local: location || "",
+            formato: format || "",
+            cargahoraria: cargahoraria || "",
             link: link || "",
           };
         });
@@ -114,6 +128,7 @@ async function getJobListings(browser, searchTerm, location, li_at) {
 
 (async () => {
   const browser = await puppeteer.launch({
+    headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
