e95f96b5fa9a8cfcdb4910770526cb7e3624a698
luisabwk <luisa.barwinski@gmail.com>
2024-11-29 12:23:13 -0300

    Update app.js - correção seletor location

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ async function getJobListings(browser, searchTerm, location, li_at) {
             ?.innerText.trim();
 
           const location = job
-            .querySelector(".job-details-jobs-unified-top-card__primary-description-container")
+            .querySelector(".job-card-container__metadata-wrapper")
             ?.innerText.trim();
 
           const link = job.querySelector("a")?.href;
