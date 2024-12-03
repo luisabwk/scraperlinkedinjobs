db88a050a0eb6736b3efed56e437223b27ff05d7
luisabwk <luisa.barwinski@gmail.com>
2024-12-03 14:01:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ app.post("/job-details", async (req, res) => {
   }
 });
 
-// Inicializar o servidor na porta 8080
-const PORT = process.env.PORT || 8080;
+// Inicializar o servidor na porta 3001
+const PORT = process.env.PORT || 3001;
 const server = app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
