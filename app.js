7c579e9ad78fa9d7ba941957bd83607606dbb660
luisabwk <luisa.barwinski@gmail.com>
2024-12-03 17:00:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,68 +6,14 @@ const getJobDetails = require("./jobs/job-details");
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // Permitir CORS para garantir requisições de outros domínios
+app.use(cors()); // Ativando CORS no Express
 
-// Endpoint /scrape-jobs
 app.post("/scrape-jobs", async (req, res) => {
-  console.log("[INFO] Recebida requisição para /scrape-jobs");
-  
-  const { searchTerm, location, li_at, maxJobs = 50 } = req.body;
-
-  if (!li_at || !searchTerm || !location) {
-    console.error("[ERROR] Parâmetros obrigatórios ausentes: 'li_at', 'searchTerm', 'location'");
-    return res.status(400).send({ error: "Parâmetros 'li_at', 'searchTerm' e 'location' são obrigatórios." });
-  }
-
-  let browser;
-  try {
-    console.log("[INFO] Iniciando navegador Puppeteer para scraping de vagas...");
-    browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu",
-        "--disable-software-rasterizer",
-      ],
-    });
-
-    console.log("[INFO] Chamando getJobListings...");
-    const jobs = await getJobListings(browser, searchTerm, location, li_at, maxJobs);
-    console.log("[INFO] Scraping realizado com sucesso! Total de vagas: ", jobs.totalVagas);
-    res.status(200).send({ message: "Scraping realizado com sucesso!", totalVagas: jobs.totalVagas, jobs: jobs.vagas });
-  } catch (error) {
-    console.error("[ERROR] Ocorreu um erro durante a requisição /scrape-jobs:", error);
-    res.status(500).send({ error: error.message });
-  } finally {
-    if (browser) {
-      await browser.close();
-      console.log("[INFO] Navegador fechado.");
-    }
-  }
+  // Código para lidar com a rota scrape-jobs
 });
 
-// Endpoint /job-details
 app.post("/job-details", async (req, res) => {
-  console.log("[INFO] Recebida requisição para /job-details");
-
-  const { jobUrl, li_at } = req.body;
-
-  if (!jobUrl || !li_at) {
-    console.error("[ERROR] Parâmetros obrigatórios ausentes: 'jobUrl', 'li_at'");
-    return res.status(400).send({ error: "Parâmetros 'jobUrl' e 'li_at' são obrigatórios." });
-  }
-
-  try {
-    console.log("[INFO] Chamando getJobDetails...");
-    const jobDetails = await getJobDetails(jobUrl, li_at);
-    console.log("[INFO] Detalhes da vaga obtidos com sucesso para URL:", jobUrl);
-    res.status(200).send({ message: "Detalhes da vaga obtidos com sucesso!", jobDetails });
-  } catch (error) {
-    console.error("[ERROR] Ocorreu um erro ao obter os detalhes da vaga:", error);
-    res.status(500).send({ error: error.message });
-  }
+  // Código para lidar com a rota job-details
 });
 
 // Inicializar o servidor na porta 8080
