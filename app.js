e74f0d6920315714f769ef4fd8210b5e0022452a
luisabwk <luisa.barwinski@gmail.com>
2024-12-04 10:00:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const cors = require("cors");
 const getJobListings = require("./jobs/scrape-jobs");
-const getJobDetails = require("./jobs/job-details");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
+// Endpoint para obter a lista de vagas
 app.post("/scrape-jobs", async (req, res) => {
   const { searchTerm, location, li_at, maxJobs = 50 } = req.body;
 
@@ -38,26 +40,20 @@ app.post("/scrape-jobs", async (req, res) => {
   }
 });
 
-app.post("/job-details", async (req, res) => {
-  const { jobUrl, li_at } = req.body;
-
-  if (!jobUrl || !li_at) {
-    return res.status(400).send({ error: "Parâmetros 'jobUrl' e 'li_at' são obrigatórios." });
-  }
-
-  try {
-    const jobDetails = await getJobDetails(jobUrl, li_at);
-    res.status(200).send({ message: "Detalhes da vaga obtidos com sucesso!", jobDetails });
-  } catch (error) {
-    console.error("[ERROR] Ocorreu um erro ao obter os detalhes da vaga:", error);
-    res.status(500).send({ error: error.message });
-  }
-});
-
-// Inicializar o servidor na porta 3001
-const PORT = process.env.PORT || 3001;
+// Inicializar o servidor na porta 8080
+const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+}).on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.log(`[WARN] Porta ${PORT} já está em uso. Tentando a próxima porta...`);
+    const newPort = PORT + 1;
+    app.listen(newPort, () => {
+      console.log(`Servidor rodando em http://localhost:${newPort}`);
+    });
+  } else {
+    console.error(`[ERROR] Ocorreu um erro ao iniciar o servidor: ${err}`);
+  }
 });
 
 // Capturar sinais de encerramento para fechar o servidor adequadamente
@@ -77,4 +73,4 @@ process.on('SIGINT', () => {
   });
 });
 
-module.exports = getJobDetails;
+module.exports = getJobListings;
