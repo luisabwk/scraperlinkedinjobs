ba53d12c4f937a71cc8dfe64ae7cbf5a0b31df99
luisabwk <luisa.barwinski@gmail.com>
2024-12-04 17:31:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
-const cors = require("cors");
 const getJobListings = require("./jobs/scrape-jobs");
 const getJobDetails = require("./jobs/job-details");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 // Endpoint para obter a lista de vagas
 app.post("/scrape-jobs", async (req, res) => {
@@ -18,7 +16,6 @@ app.post("/scrape-jobs", async (req, res) => {
 
   let browser;
   try {
-    console.log("[DEBUG] Iniciando scraping de jobs...");
     browser = await puppeteer.launch({
       headless: true,
       args: [
@@ -32,14 +29,9 @@ app.post("/scrape-jobs", async (req, res) => {
 
     const jobs = await getJobListings(browser, searchTerm, location, li_at, maxJobs);
     res.status(200).send({ message: "Scraping realizado com sucesso!", totalVagas: jobs.totalVagas, jobs: jobs.vagas });
-
-    // Encerrar a aplicação logo após enviar a resposta
-    console.log("[INFO] Encerrando aplicação após conclusão do endpoint /scrape-jobs...");
-    process.exit(0);
   } catch (error) {
     console.error("[ERROR] Ocorreu um erro:", error);
     res.status(500).send({ error: error.message });
-    process.exit(1); // Encerrar com código de erro
   } finally {
     if (browser) {
       await browser.close();
@@ -57,7 +49,6 @@ app.post("/job-details", async (req, res) => {
 
   let browser;
   try {
-    console.log("[DEBUG] Iniciando scraping de detalhes da vaga...");
     browser = await puppeteer.launch({
       headless: true,
       args: [
@@ -71,14 +62,9 @@ app.post("/job-details", async (req, res) => {
 
     const jobDetails = await getJobDetails(browser, jobUrl, li_at);
     res.status(200).send({ message: "Detalhes da vaga obtidos com sucesso!", jobDetails });
-
-    // Encerrar a aplicação logo após enviar a resposta
-    console.log("[INFO] Encerrando aplicação após conclusão do endpoint /job-details...");
-    process.exit(0);
   } catch (error) {
     console.error("[ERROR] Ocorreu um erro ao obter os detalhes da vaga:", error);
     res.status(500).send({ error: error.message });
-    process.exit(1); // Encerrar com código de erro
   } finally {
     if (browser) {
       await browser.close();
@@ -88,39 +74,8 @@ app.post("/job-details", async (req, res) => {
 
 // Inicializar o servidor na porta 8080
 const PORT = process.env.PORT || 8080;
-let server;
-
-const startServer = (port) => {
-  server = app.listen(port, () => {
-    console.log(`Servidor rodando em http://api.growthbrains.com.br:${port}`);
-  }).on('error', (err) => {
-    if (err.code === 'EADDRINUSE') {
-      console.log(`[WARN] Porta ${port} já está em uso. Tentando uma porta aleatória...`);
-      const newPort = Math.floor(Math.random() * (65535 - 3000)) + 3000; // Escolhe uma porta aleatória entre 3000 e 65535
-      startServer(newPort);
-    } else {
-      console.error(`[ERROR] Ocorreu um erro ao iniciar o servidor: ${err}`);
-      process.exit(1);
-    }
-  });
-};
-
-startServer(PORT);
-
-// Capturar sinais de encerramento para fechar o servidor adequadamente
-const gracefulShutdown = () => {
-  console.log("Encerrando servidor...");
-  if (server) {
-    server.close(() => {
-      console.log("Servidor encerrado.");
-      process.exit(0);
-    });
-  } else {
-    process.exit(0);
-  }
-};
-
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGINT', gracefulShutdown);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
 
 module.exports = { getJobListings, getJobDetails };
