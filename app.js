adff3e4a1d54217037e4df991f4df4746ce0ed68
luisabwk <luisa.barwinski@gmail.com>
2024-12-04 17:34:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,9 +14,8 @@ app.post("/scrape-jobs", async (req, res) => {
     return res.status(400).send({ error: "Parâmetros 'li_at', 'searchTerm' e 'location' são obrigatórios." });
   }
 
-  let browser;
   try {
-    browser = await puppeteer.launch({
+    const browser = await puppeteer.launch({
       headless: true,
       args: [
         "--no-sandbox",
@@ -28,14 +27,11 @@ app.post("/scrape-jobs", async (req, res) => {
     });
 
     const jobs = await getJobListings(browser, searchTerm, location, li_at, maxJobs);
+    await browser.close();
     res.status(200).send({ message: "Scraping realizado com sucesso!", totalVagas: jobs.totalVagas, jobs: jobs.vagas });
   } catch (error) {
     console.error("[ERROR] Ocorreu um erro:", error);
     res.status(500).send({ error: error.message });
-  } finally {
-    if (browser) {
-      await browser.close();
-    }
   }
 });
 
@@ -47,9 +43,8 @@ app.post("/job-details", async (req, res) => {
     return res.status(400).send({ error: "Parâmetros 'jobUrl' e 'li_at' são obrigatórios." });
   }
 
-  let browser;
   try {
-    browser = await puppeteer.launch({
+    const browser = await puppeteer.launch({
       headless: true,
       args: [
         "--no-sandbox",
@@ -61,21 +56,30 @@ app.post("/job-details", async (req, res) => {
     });
 
     const jobDetails = await getJobDetails(browser, jobUrl, li_at);
+    await browser.close();
     res.status(200).send({ message: "Detalhes da vaga obtidos com sucesso!", jobDetails });
   } catch (error) {
     console.error("[ERROR] Ocorreu um erro ao obter os detalhes da vaga:", error);
     res.status(500).send({ error: error.message });
-  } finally {
-    if (browser) {
-      await browser.close();
-    }
   }
 });
 
 // Inicializar o servidor na porta 8080
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+function startServer(port) {
+  const server = app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+  });
+
+  server.on("error", (error) => {
+    if (error.code === "EADDRINUSE") {
+      console.warn(`[WARN] Porta ${port} já está em uso. Tentando a próxima porta...`);
+      startServer(port + 1); // Tenta a próxima porta
+    } else {
+      console.error("[ERROR] Erro ao iniciar o servidor:", error);
+    }
+  });
+}
+
+startServer(8080);
 
 module.exports = { getJobListings, getJobDetails };
